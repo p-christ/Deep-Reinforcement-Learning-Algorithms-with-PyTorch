739f95b6707593326ac2a7f11b7aaabb2dad57d7
p-christ <p.christodoulou2@gmail.com>
2019-01-07 19:54:26 +0000

    updated bit flipping tests

--- Tests/Test_Bit_Flipping_Environment.py
+++ Tests/Test_Bit_Flipping_Environment.py
@@ -31,23 +31,23 @@ def test_environment_goal_achievement():
     env.desired_goal = [0, 0, 0, 0, 0]
 
     env.conduct_action(0)
-    assert env.reward == 0
+    assert env.reward == -1
     env.state = env.next_state
 
     assert env.get_achieved_goal() == [0, 0, 0, 1, 0]
 
     env.conduct_action(2)
-    assert env.reward == 0
+    assert env.reward == -1
     env.state = env.next_state
 
     assert env.get_achieved_goal() == [0, 0, 1, 1, 0]
 
     env.conduct_action(2)
-    assert env.reward == 0
+    assert env.reward == -1
     env.state = env.next_state
 
     assert env.get_achieved_goal() == [0, 0, 0, 1, 0]
 
     env.conduct_action(3)
-    assert env.reward == 1
+    assert env.reward == 5
 
