be8d77e6ecb542f36b8dc0775a6a3e922c1b2660
p-christ <p.christodoulou2@gmail.com>
2019-04-24 16:05:04 +0100

    completed tests for HIRO

--- Tests/Test_HIRO.py
+++ Tests/Test_HIRO.py
@@ -161,74 +161,50 @@ def test_changing_max_lower_timesteps():
     assert hiro_agent2.lower_level_done
     assert reward == hiro_agent2.higher_level_reward
 
+def test_lower_level_step():
+    """Tests the step level for the lower level environment"""
+    h_env.reset()
+    state = ll_env.reset()
+    assert state.shape[0] == 6
 
-def test_sub_policy_env_reset():
-    """Tests reset method in the sub policy environment we create"""
-    external_state = env.reset()
-    assert external_state.shape[0] == 6
-    assert env.internal_state.shape[0] == 3
-    assert all(env.goal == agent.give_next_goal_for_sub_policy())
-    assert not env.episode_over
-    assert env.timesteps == 0
-    assert env.max_sub_policy_timesteps == 5
+    hl_next_state, reward, done, _ = h_env.step(np.array([-1.0, 2.0, 3.0]))
+
+    assert all(hl_next_state == hiro_agent.lower_level_next_state[:3])
+    assert all(hl_next_state == hiro_agent.lower_level_state[:3])
+    assert hiro_agent.lower_level_done
+    assert ll_env.lower_level_timesteps == 3
+
+    next_state, reward, done, _ = ll_env.step(np.array([-1.0]))
+
+    assert next_state.shape[0] == 6
+    assert all(next_state[3:] == hiro_agent.goal)
+    assert all(next_state[:3] == hiro_agent.higher_level_next_state)
+    assert all(next_state[:3] == hiro_agent.higher_level_state)
+    assert all(next_state == hiro_agent.lower_level_next_state)
+    assert all(next_state == hiro_agent.lower_level_state)
+    assert done == hiro_agent.lower_level_done
+    assert not hiro_agent.higher_level_done
+    assert reward == ll_env.calculate_intrinsic_reward(hl_next_state,  hiro_agent.higher_level_next_state , hiro_agent.goal)
+
+    for _ in range(100):
+        state = hiro_agent.higher_level_next_state
+        next_state, reward, done, _ = ll_env.step(np.array([random.random()]))
+        assert next_state.shape[0] == 6
+        assert all(next_state[3:] == hiro_agent.goal)
+        assert all(next_state[:3] == hiro_agent.higher_level_next_state)
+        assert all(next_state[:3] == hiro_agent.higher_level_state)
+        assert all(next_state == hiro_agent.lower_level_next_state)
+        assert all(next_state == hiro_agent.lower_level_state)
+        assert done == hiro_agent.lower_level_done
+        assert reward == ll_env.calculate_intrinsic_reward(state, hiro_agent.higher_level_next_state,
+                                                           hiro_agent.goal)
 
 def test_sub_policy_env_turn_internal_state_to_external_state():
     """Tests turn_internal_state_to_external_state method in the sub policy environment we create"""
-    env.goal = np.array([1., 2., 3.])
-    result = env.turn_internal_state_to_external_state(np.array([9., 9., 9.]))
+    goal = np.array([1., 2., 3.])
+    result = ll_env.turn_internal_state_to_external_state(np.array([9., 9., 9.]), goal)
     assert all(result == np.array([9., 9., 9., 1., 2., 3.]))
-    external_state = env.reset()
-    env.goal = np.array([-1., 0., 0.])
-    result = env.turn_internal_state_to_external_state(external_state[0:3])
+    external_state = ll_env.reset()
+    goal = np.array([-1., 0., 0.])
+    result = ll_env.turn_internal_state_to_external_state(external_state[0:3], goal)
     assert all(result[:3] == external_state[:3]) and all(result[3:] == np.array([-1., 0., 0.]))
-
-def test_sub_policy_env_step():
-    """Tests step method in the sub policy environment we create"""
-    assert len(agent.extrinsic_rewards) == 0
-    external_state = env.reset()
-    next_external_state, intrinsic_reward, episode_over, _ = env.step(np.array([0.0]))
-    assert next_external_state.shape[0] == 6
-    assert all(next_external_state[3:] == external_state[3:])
-    assert len(agent.extrinsic_rewards) == 1
-    assert env.timesteps == 1
-    env.step(np.array([0.0]))
-    assert env.timesteps == 2
-
-    external_state = env.reset()
-    assert env.timesteps == 0
-    env.goal = np.array([0.0, 5.0, -22.0])
-    next_external_state, intrinsic_reward, episode_over, _ = env.step(np.array([0.0]))
-    assert intrinsic_reward == env.calculate_intrinsic_reward(external_state[0:3], next_external_state[0:3], env.goal)
-    assert all(env.internal_next_state == next_external_state[0:3])
-    assert env.timesteps == 1
-    assert not env.episode_over
-
-    external_state = env.reset()
-    for _ in range(4):
-        next_external_state, intrinsic_reward, sub_policy_episode_over, _ = env.step(np.array([0.0]))
-        assert not env.episode_over
-        assert not sub_policy_episode_over
-
-    next_external_state, intrinsic_reward, sub_policy_episode_over, _ = env.step(np.array([0.0]))
-    assert sub_policy_episode_over
-    assert not env.episode_over
-
-    for _ in range(1000):
-        env.step(np.array([0.0]))
-
-    assert env.episode_over
-    assert env.sub_policy_episode_over
-
-def test_sub_policy_env_calculate_intrinsic_reward():
-    """Tests calculate_intrinsic_reward in the sub policy environment we create"""
-    assert env.calculate_intrinsic_reward(5, 9, 4) == 0
-    assert env.calculate_intrinsic_reward(5, 9, 1.5) == -(2.5**2)**0.5
-    state = np.array([0.5, -3.5])
-    next_state = np.array([3.5, 23.5])
-    goal = np.array([-23.5, 15.5])
-    combined = state + goal - next_state
-    result = -np.linalg.norm(combined)
-    assert np.allclose(env.calculate_intrinsic_reward(state, next_state, goal), result)
-
-
-
