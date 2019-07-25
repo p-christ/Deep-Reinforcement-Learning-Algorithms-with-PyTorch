fixed numerical stability issue with SAC discrete

agents/actor_critic_agents/SAC_Discrete.py
@@ -55,7 +55,10 @@ class SAC_Discrete(SAC):
         max_probability_action = torch.argmax(action_probabilities).unsqueeze(0)
         action_distribution = create_actor_distribution(self.action_types, action_probabilities, self.action_size)
         action = action_distribution.sample().cpu()
-        log_action_probabilities = torch.log(action_probabilities)
+        # Have to deal with situation of 0.0 probabilities because we can't do log 0
+        z = action_probabilities == 0.0
+        z = z.float() * 1e-8
+        log_action_probabilities = torch.log(action_probabilities + z)
         return action, (action_probabilities, log_action_probabilities), max_probability_action
 
     def calculate_critic_losses(self, state_batch, action_batch, reward_batch, next_state_batch, mask_batch):