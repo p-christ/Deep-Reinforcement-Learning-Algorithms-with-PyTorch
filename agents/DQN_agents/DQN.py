0f964443c152cc6e812ea2909a94c6be9c087ed8
half-empty <122198841@qq.com>
2020-08-20 11:26:32 +0800

    fixed an error with using cpu
    keep the same device as config

--- agents/DQN_agents/DQN.py
+++ agents/DQN_agents/DQN.py
@@ -14,7 +14,7 @@ class DQN(Base_Agent):
     agent_name = "DQN"
     def __init__(self, config):
         Base_Agent.__init__(self, config)
-        self.memory = Replay_Buffer(self.hyperparameters["buffer_size"], self.hyperparameters["batch_size"], config.seed)
+        self.memory = Replay_Buffer(self.hyperparameters["buffer_size"], self.hyperparameters["batch_size"], config.seed, self.device)
         self.q_network_local = self.create_NN(input_dim=self.state_size, output_dim=self.action_size)
         self.q_network_optimizer = optim.Adam(self.q_network_local.parameters(),
                                               lr=self.hyperparameters["learning_rate"], eps=1e-4)
